6ab70624378ba47960b42768e082bca39e8b27e8
Michael Mascaro <michael.mascaro@ai-solutions.com>
2011-06-21 15:37:19 -0400

    Added custom satellite class to WWAPP and required functions, made necessary changes to prevent errors and allow the applet to still run.

---
+++ src/Layers/ECIRenderableLayer.java
@@ -0,0 +1,163 @@
+/**
+ * =====================================================================
+ * Copyright (C) 2009 Shawn E. Gano
+ * 
+ * This file is part of JSatTrak.
+ * 
+ * JSatTrak is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * JSatTrak is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JSatTrak.  If not, see <http://www.gnu.org/licenses/>.
+ * =====================================================================
+ */
+package Layers;
+
+import gov.nasa.worldwind.layers.RenderableLayer;
+import gov.nasa.worldwind.render.DrawContext;
+import gov.nasa.worldwind.render.Renderable;
+import Layers.OrbitModelRenderable;
+
+/**
+ * This layer automatically rotates its collection of renderables so that their 
+ * coordinates are plotted in ECI coordinates based on current time (MDJ).
+ * <br><i>Note:</i> Because of the JOGL coordinate system is different than the ECI (e.g. J2000.0) coordinate system
+ *       the renderables added to this collection should plot the ECI coordinates (x,y,z) as ( -x, z, y) 
+ *       For example: gl.glVertex3d( -x, z , y);
+ *
+ * <p><b>!!Precession and Nutation are not yet accouted for.</b>
+ *
+ * <p>Shawn E. Gano
+ * <br>Created on October 12, 2007 - update May 5 2008
+ *
+ * 
+ * @author Shawn E. Gano (shawn@gano.name)
+ * @version $id$
+ */
+public class ECIRenderableLayer extends RenderableLayer
+{
+    
+    
+    // ECI rotation variable
+    /**
+     * jogl coordinate Greenwich to ECI x-axis offset
+     */
+    public static final double offsetRotdeg = -90.0; // jogl coordinate Greenwich to ECI x-axis offset
+    private double rotateECIdeg = 280.46061837+offsetRotdeg; // rotation in degrees (default j2k)
+    private double currentMJD = 51544.5; // current modified julian date, universal time (default J2k)
+
+    
+    // SEG added -------------
+    /**
+     * Creates layer with initial time
+     * @param iniMJD inital Modified Julian Date
+     */
+    public ECIRenderableLayer(double iniMJD)
+    {
+        super();
+        //this.delegateOwner = null;
+        setCurrentMJD(iniMJD);
+    } // ECIRenderableLayer - constructor
+
+//    public ECIRenderableLayer(Layer delegateOwner)
+//    {
+//        this.delegateOwner = delegateOwner;
+//    }
+    
+//    // SEG added -------------
+//    /**
+//     * Creates layer with delagate layer owner and initial time
+//     * @param delegateOwner delegate owner
+//     * @param iniMJD inital Modified Julian Date
+//     */
+//    public ECIRenderableLayer(Layer delegateOwner, double iniMJD)
+//    {
+//        this.delegateOwner = delegateOwner;
+//        setCurrentMJD(iniMJD);
+//    } // ECIRenderableLayer - constructor
+
+    // SEG modified -------------
+    @Override
+    //  This method has been modified to include rotating for ECI coordinates based on current MJD
+    protected void doRender(DrawContext dc)
+    {
+        javax.media.opengl.GL gl = dc.getGL();
+        
+        // this line must be before matrix push - otherwise when coverage is off, an ECI is on EFEF lines don't show
+       gl.glMatrixMode(javax.media.opengl.GL.GL_MODELVIEW); // add to prevent interatction with star layer // MUST INCLUDE THIS -- 5 May 2008 SEG
+        
+        gl.glPushMatrix();   // push for ECI roation
+        gl.glRotated(-rotateECIdeg, 0.0, 1.0, 0.0); // rotate about Earth's spin axis (z-coordinate in J2K, y-coordinate in JOGL)
+         
+        for (Renderable renderable : super.getRenderables())
+        {
+            // If the caller has specified their own Iterable,
+            // then we cannot make any guarantees about its contents.
+            if (renderable != null)
+            {
+                renderable.render(dc);
+            }
+        }
+        
+        gl.glPopMatrix(); // pop matrix rotatex for ECI
+        
+    } // do Render
+
+
+    @Override
+    public String toString()
+    {
+        return "ECI Layer";
+    }
+
+    // SEG added -------------
+    public double getCurrentMJD()
+    {
+        return currentMJD;
+    } // getCurrentMJD
+
+    // SEG added -------------
+    /**
+     * Set the time for the ECI layer.  
+     * Mean Sidereal formula taken from <i>Astronomical Algorithms</i> 2nd ed., Jean Meesus. 
+     *
+     * @param currentMJD  modified Julian Date (Universal Time)
+     */
+    public void setCurrentMJD(double currentMJD)
+    {
+        this.currentMJD = currentMJD;
+        
+        // centuries since J2000.0
+        double T = (currentMJD-51544.5)/36525.0;  
+        
+        // now calculate the mean sidereal time at Greenwich (UT time) in degrees
+        rotateECIdeg =  ( (280.46061837 + 360.98564736629*(currentMJD-51544.5)) + 0.000387933*T*T - T*T*T/38710000.0 +offsetRotdeg) % 360.0;
+        
+        //System.out.println("Rotat:" + rotateECIdeg);
+        
+        // set ECI angle to all OrbitModelRenderables
+         for (Renderable renderable : super.getRenderables())
+        {
+             if(renderable instanceof OrbitModelRenderable)
+             {
+                 ((OrbitModelRenderable)renderable).updateMJD(currentMJD, rotateECIdeg);
+             }//OrbitModelRenderable
+         } // for renderables
+        
+    } // setCurrentMJD
+
+    // SEG added -------------
+    public double getRotateECIdeg()
+    {
+        return rotateECIdeg;
+    } //getRotateECIdeg
+    
+    
+}
---
+++ src/Shapes/Cone.java
@@ -0,0 +1,163 @@
+/*
+ * Orginal code from: http://forum.worldwindcentral.com/showthread.php?t=13111
+ * Updates by: Shawn E. Gano
+ *  - Updated 24 Sept 2008 - added get and set for slices and stacks (and changed default)
+ * =====================================================================
+ * Copyright (C) 2009 Shawn E. Gano
+ * 
+ * This file is part of JSatTrak.
+ * 
+ * JSatTrak is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * JSatTrak is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JSatTrak.  If not, see <http://www.gnu.org/licenses/>.
+ * =====================================================================
+ */
+
+package Shapes;
+
+import gov.nasa.worldwind.geom.Angle;
+import gov.nasa.worldwind.geom.Position;
+import gov.nasa.worldwind.geom.Vec4;
+import gov.nasa.worldwind.globes.Globe;
+import gov.nasa.worldwind.render.DrawContext;
+import gov.nasa.worldwind.render.Renderable;
+import java.awt.Color;
+import javax.media.opengl.GL;
+import javax.media.opengl.glu.GLU;
+import javax.media.opengl.glu.GLUquadric;
+
+public class Cone implements Renderable
+{
+
+    private Angle myPointLat;
+    private Angle myPointLon;
+    private Angle myOrientation; // degrees tilted counterclockwise from due East
+    private Angle myElevation; // degrees tilted up from the surface
+    private double myGroundRange;  // radius of cone base
+    private double myCeiling; // length from cone tip to center of base
+    private Globe myGlobe;
+    private Color color;
+    private Vec4 topCenter; // point at center of cylinder base
+    
+    // solid geometry generation parameters
+    private int slices = 30;  // 30
+    private int stacks = 1;  // 30
+    
+
+    public Cone(Globe globe, double lat, double lon, double mGroundRange, double mCeiling, Angle orientation, Angle elevation, Color theColor)
+    {
+
+        this.myPointLat = Angle.fromDegreesLatitude(lat);
+        this.myPointLon = Angle.fromDegreesLongitude(lon);
+        this.myGlobe = globe;
+        this.topCenter = myGlobe.computePointFromPosition(myPointLat, myPointLon, 0.0);
+        this.myOrientation = orientation;
+        this.myElevation = elevation;
+        this.myGroundRange = mGroundRange;
+        this.myCeiling = mCeiling;
+        this.color = theColor;
+
+    }
+
+    public void render(DrawContext dc)
+    {
+
+        Position p = myGlobe.computePositionFromPoint(this.topCenter);
+
+        javax.media.opengl.GL gl = dc.getGL();
+
+        gl.glPushAttrib(javax.media.opengl.GL.GL_TEXTURE_BIT | javax.media.opengl.GL.GL_ENABLE_BIT | javax.media.opengl.GL.GL_CURRENT_BIT);
+        gl.glDisable(javax.media.opengl.GL.GL_TEXTURE_2D);
+
+        gl.glEnable(GL.GL_BLEND);
+        gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
+        gl.glColor4ub((byte) getColor().getRed(), (byte) getColor().getGreen(), (byte) getColor().getBlue(), (byte) getColor().getAlpha());
+
+        gl.glEnable(javax.media.opengl.GL.GL_DEPTH_TEST);
+        gl.glMatrixMode(javax.media.opengl.GL.GL_MODELVIEW);
+        gl.glPushMatrix();
+
+        gl.glTranslated(this.topCenter.x, this.topCenter.y, this.topCenter.z);
+
+        dc.getGL().glRotated(90 + p.getLongitude().getDegrees(), 0, 1, 0);
+        dc.getGL().glRotated(myOrientation.getDegrees(), -1, 0, 0);
+        dc.getGL().glRotated(p.getLatitude().getDegrees() * myOrientation.sin(), 0, 1, 0);
+        dc.getGL().glRotated(myElevation.getDegrees(), 0, -1, 0);
+        dc.getGL().glRotated(p.getLatitude().getDegrees() * myElevation.sin(), -1, 0, 0);
+
+        GLUquadric quadric = dc.getGLU().gluNewQuadric();
+        dc.getGLU().gluQuadricDrawStyle(quadric, GLU.GLU_FILL);
+
+        dc.getGLU().gluCylinder(quadric, 0, this.myGroundRange, this.myCeiling, slices, stacks);
+        dc.getGL().glTranslated(0, 0, this.myCeiling);
+        dc.getGLU().gluDisk(quadric, 0d, this.myGroundRange, slices, stacks);
+        dc.getGLU().gluDeleteQuadric(quadric);
+
+        gl.glPopMatrix();
+        gl.glPopAttrib();
+
+    }
+
+    public void setVertexPosition(double x, double y, double z)
+    {
+        this.topCenter = new Vec4(x,y,z,0);
+        
+    }
+    
+    public void setLatLonRadians(double lat, double lon, double alt)
+    {
+        this.myPointLat = Angle.fromRadiansLatitude(lat);
+        this.myPointLon = Angle.fromRadiansLatitude(lon);
+        this.topCenter = myGlobe.computePointFromPosition(myPointLat, myPointLon, alt);
+        
+    }
+
+    // corrected spelling SEG 30 Mar 2010
+    public void setLatitudeRadians(double latRad)
+    {
+        this.myPointLat = Angle.fromRadiansLatitude(latRad);
+        this.topCenter = myGlobe.computePointFromPosition(myPointLat, myPointLon, 0.0);
+    }
+
+    // old typo - for backwards compatibility (thanks csuski - from the forum)
+    public void setLatitudeReadians(double latRad)
+    {
+        setLatitudeRadians(latRad);
+    }
+
+
+    public void setLongitudeRadians(double longitude)
+    {
+        this.myPointLon = Angle.fromRadiansLatitude(longitude);
+        this.topCenter = myGlobe.computePointFromPosition(myPointLat, myPointLon, 0.0);
+    }
+
+    public void setGroundRange(double myGroundRange)
+    {
+        this.myGroundRange = myGroundRange;
+    }
+
+    public void setHeight(double myCeiling)
+    {
+        this.myCeiling = myCeiling;
+    }
+
+    public Color getColor()
+    {
+        return color;
+    }
+
+    public void setColor(Color color)
+    {
+        this.color = color;
+    }
+}
