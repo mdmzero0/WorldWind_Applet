0d696fd344fe9ac8d9e7aef154989e1680a35e55
Sean <seankli123@gmail.com>
2011-07-22 11:03:53 -0400

    Premerge with

--- src/Main/WWJApplet.java
+++ src/Main/WWJApplet.java
@@ -1312,7 +1312,7 @@ public void WWsetMJD(double mjd)
     {
             //Read satellites
             try{
-            input = new OnlineInput("http://localhost:8080/parameters_test.html");
+            input = new OnlineInput("http://localhost:8080/sean.html");
             int n = input.getSize();
             for (int i = 0; i <n; i++)
             {
